72bdc595b9f124571089430ce3ee5a4646241248
Ajay Narsimha M <ajaymopidevi@gmail.com>
2021-08-18 22:55:20 +0530

    Update eightpoint algorithm

--- matlab/eightpoint.m
+++ matlab/eightpoint.m
@@ -1,12 +1,8 @@
 function F = eightpoint(pts1, pts2, M)
-% eightpoint:
+% eightpoint algorithm:
 %   pts1 - Nx2 matrix of (x,y) coordinates
 %   pts2 - Nx2 matrix of (x,y) coordinates
 %   M    - max (imwidth, imheight)
-
-% Q2.1 - Todo:
-%     Implement the eightpoint algorithm
-%     Generate a matrix F from correspondence '../data/some_corresp.mat'
 
 N = size(pts1,1);
 A = zeros(N,9);
@@ -18,8 +14,7 @@ for i=1:N
   v = pts2(i,2)/M;
   
   A(i,:) = [u*x, u*y, u, v*x, v*y, v, x, y, 1];
-  %A(i,:) = [x*u, x*v, x, y*u, y*v, y, u, v, 1];
-
+  
 end
 
 
@@ -34,15 +29,8 @@ S(3,3)=0;
 F=U*S*V';
 
 
-%T = eye(3);
-%T(1,1)=1/M;
-%T(2,2)=1/M;
 T = [1/M, 0, 0; 0, 1/M, 0; 0, 0, 1/M];
 F = T'*F*T;
-disp(F);
-%pts1 = pts1./M;
-%pts2 = pts2./M;
+
 F = refineF(F,pts1, pts2);
-disp(F)
-%F = F.*(M*M);
 end 
