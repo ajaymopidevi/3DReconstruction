d9af83c8bd555e0c31abbf83db3648a30bfd6870
Ajay Narsimha M <ajaymopidevi@gmail.com>
2021-08-24 22:24:29 +0530

    Update get_disparity

--- matlab/get_disparity.m
+++ matlab/get_disparity.m
@@ -4,8 +4,7 @@ function dispM = get_disparity(im1, im2, maxDisp, windowSize)
 
 sz1 = size(im1);
 sz2 = size(im2);
-disp(sz1);
-disp(sz2);
+
 im1 = im2double(im1);
 im2 = im2double(im2);
 
@@ -16,17 +15,16 @@ xend = sz1(2) - w;
 ystart = w+1;
 yend = sz1(1) -w;
 dispM = zeros(sz1(1), sz1(2));
+
 for y=ystart:yend
   for x= xstart:xend
     im1_patch = im1(y-w:y+w,x-w:x+w);
     disparity = ones(xend-xstart+1,1)*Inf;
-    %disp(size(disparity));
-    for idx = xstart:xend
+    
+	for idx = xstart:xend
       im2_patch = im2(y-w:y+w, idx-w:idx+w);
-      %disp(sum(sum(im2_patch)));
       diff = sum(sum((im2_patch - im1_patch).**2));
       disparity(idx) = diff;
-      %disparity(idx) = disparity(idx)**2;
       
     endfor
     [v,i] = min(disparity);
